de5b0a2942981bf319dff5c8f7c027fd54eb74fe
IgorZaytsev <zayt.2002@mail.ru>
2023-03-03 03:06:56 +0300

    Added function copyToClipboard for mail button

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function copyToClipboard() {
+    const str = document.getElementById('copyText').innerText;
+    const el = document.createElement('textarea');
+    el.value = str;
+    el.setAttribute('readonly', '');
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+}
