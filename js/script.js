925f058aa5046cfc2a9de5c6c74d1f98846a5bd6
IgorZaytsev <zayt.2002@mail.ru>
2023-03-05 05:00:17 +0300

    Replaced the onclipBoard function with the SendEmail function

--- js/script.js
+++ js/script.js
@@ -1,27 +1,6 @@
-function copyToClipboard() {
-    const str = document.getElementById('copyText').innerText;
-    const el = document.createElement('textarea');
-    el.value = str;
-    el.setAttribute('readonly', '');
-    el.style.position = 'absolute';
-    el.style.left = '-9999px';
-    document.body.appendChild(el);
-    el.select();
-    document.execCommand('copy');
-    document.body.removeChild(el);
-    
-    const button = document.getElementById("copyText");
-    const text = button.innerText;
-    navigator.clipboard.writeText(text);
-    const notification = document.createElement('span');
-    notification.classList.add('copy-notification');
-    notification.textContent = 'Текст скопирован в буфер обмена'; 
-    button.parentNode.insertBefore(notification, button.nextSibling); 
-    setTimeout(() => notification.remove(), 2000);
-      
-
-  
-    }
+function sendEmail() {
+    location.href = 'mailto:zayt.2002@mail.ru?subject=Вопрос%20по%20сотрудничеству';
+}
 
 
     
